15e1878b923419d0b11bc37b5ec9457e4aca615e
vivekkumar333 <tiwari.vivekkumar333@gmail.com>
2025-08-16 15:03:22 +0530

    Added pms-api-gateway code to use for multibranch pipeline

---
+++ src/main/java/com/vkt/pms/CpsApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.vkt.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpsApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpsApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vkt/pms/CpsApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkt.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpsApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
